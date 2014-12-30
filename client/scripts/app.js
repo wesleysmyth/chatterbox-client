7057aa2d6b925d998f96b90c47e756fcf749b1f7
wesleysmyth <wesleytatesmith@gmail.com>
2014-12-29 17:33:08 -0800

    Adds ability to filter data received from GET request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,7 @@
 // YOUR CODE HERE:
 
 var message = {
-  'username': 'shawndrost',
+  'username': window.location.search.slice(10),
   'text': 'trololo',
   'roomname': '4chan'
 };
@@ -14,6 +14,7 @@ $.ajax({
   contentType: 'application/json',
   success: function (data) {
     console.log('chatterbox: Message sent');
+    console.dir(data);
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -22,34 +23,40 @@ $.ajax({
 });
 
 
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message received');
-    console.log(data);
-    // grab data from server, loop through array of objects
-    var messages = data.results;
-    // for each object, select what we want to display on the screen
-    for (var i = 0; i < messages.length; i++) {
-      var newMessage = document.createElement('div');
-      // create an element and set its' innerText to the data associated with that object
-      newMessage.innerText = messages[i].username + ': ' + messages[i].text;
-      // append element to h1
-      $('#main').append(newMessage);
-    }
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to receive message');
-  }
-});
+var getMessage = function(){
+ $.ajax({
+   // always use this url
+   url: 'https://api.parse.com/1/classes/chatterbox/',
+   type: 'GET',
+   contentType: 'application/json',
+   data: {order: '-createdAt', limit: 10},
+   success: function (data) {
+     console.log('chatterbox: Message received');
+     console.dir(data);
+     //grab data from server, loop through array of objects
+     var messages = data.results;
+     // for each object, select what we want to display on the screen
+     for (var i = 0; i < messages.length; i++) {
+       var newMessage = document.createElement('div');
+       // create an element and set its' innerText to the data associated with that object
+       newMessage.innerText = messages[i].username + ': ' + messages[i].text;
+       // append element to h1
+       $('#main').append(newMessage);
+     }
+   },
+   error: function (data) {
+     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+     console.error('chatterbox: Failed to receive message');
+   }
+ });
+};
 
+getMessage();
 
-
+// set interval to
+/*setInterval(function() {
+  getMessage();
+}, 3000)*/
 
 
 
